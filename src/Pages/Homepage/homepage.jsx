90dde1dd60d1c99d3e813cb3871ae876bdea5662
Akshay Rana <akshayrana855@akshays-air.home>
2024-05-28 12:54:29 -0400

    First commit after setup

---
+++ src/Pages/Homepage/homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function homepage() {
+  return (
+    <div className='container'>
+      <h1>
+        Hello This is homepage
+      </h1>
+    </div>
+  )
+}
+
+export default homepage
