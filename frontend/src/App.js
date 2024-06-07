a66919aba34c750bee20ac35e6a17677e4908f78
prashanttanwar124 <prashanttanwar124@gmail.com>
2024-06-06 21:00:14 -0400

    frontend Structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,112 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup';
+import JobList from './components/JobList/jobList';
+import Nav from './components/Header/Header';
+import Footer from './components/Footer/footer';
+import Homepage from './components/Homepage/homepage';
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Home from './Pages/Homepage/homepage';
+
+import { useState } from 'react';
+
+import { AuthProvider, useAuth } from './useToken';
+
+const Layout = ({ children }) => {
+  useEffect(() => {
+    const body = document.querySelector('#root');
+    body.scrollIntoView(
+      {
+        behavior: 'smooth',
+      },
+      500
+    );
+  });
+
+  return (
+    <section>
+      <Nav />
+      {children}
+      <Footer />
+    </section>
+  );
+};
+
+// TODO:
+// const AdminLayout = ({ children }) => {
+//   const { token, userInfo } = useAuth();
+
+//   useEffect(() => {
+//     if (!token || !userInfo || !userInfo.userType) {
+//       return (window.location.href = '/login');
+//     }
+//   }, [token, userInfo]);
+
+//   return (
+//     <div>
+//       <div className='content-padder content-background'>{children}</div>
+//     </div>
+//   );
+// };
+
+function Home() {
+  return (
+    <div>
+      <BrowserRouter>
+        <RouteMain />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+function RouteMain() {
+  return (
+    <div>
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <Layout>
+              <Homepage />
+            </Layout>
+          }
+        />
+        <Route
+          path='/login'
+          element={
+            <Layout>
+              <Login />
+            </Layout>
+          }
+        />
+        <Route
+          path='/signup'
+          element={
+            <Layout>
+              <Signup />
+            </Layout>
+          }
+        />
+        <Route
+          path='/job-list'
+          element={
+            <Layout>
+              <JobList />
+            </Layout>
+          }
+        />
+      </Routes>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <>
-       <Routes>
-          <Route path="/" element={<Home />} />
-       </Routes>
-    </>
+    <div className='App'>
+      <AuthProvider>
+        <Home />
+      </AuthProvider>
+    </div>
   );
 }
 
