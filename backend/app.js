ab76e364b5701cdfd405255852f42d8c8be7c70f
prashanttanwar124 <prashanttanwar124@gmail.com>
2024-08-14 20:02:18 -0400

    test for merging

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ dotenv.config({ path: './.env' });
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
+// Middleware test
 app.use(bodyParser.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
